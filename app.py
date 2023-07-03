eff189098a64a53a7a2238381f88df7b86ed4765
Deepak Pareek <deep.pareek31@gmail.com>
2023-07-03 12:43:58 +0530

    This is the commit for web application

---
+++ app.py
@@ -0,0 +1,26 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+reg_model=pickle.load(open('Linear_reg_model.pkl','rb'))
+scaler=pickle.load(open('Standard_Scaler.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    ##the input we gave in form of jason is captured by 'data' key and when 
+    ##when we hit the predict_api ,the data present in 'data' will be captured by reqst.json
+    data=request.json['data']  
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=reg_model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
